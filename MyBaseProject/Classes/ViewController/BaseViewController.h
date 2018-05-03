890e317b72019772323b7817272458b844764686
renb <91renb@gmail.com>
2018-05-03 15:48:48 +0800

    备份iOS基本项目架构

---
+++ MyBaseProject/Classes/ViewController/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  MyBaseProject
+//
+//  Created by 任波 on 16/12/10.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
